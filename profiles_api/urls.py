4a83cc0c8e00e546276c86d4b4775f380a35aafb
Adipt <adipt.dev.tomar@gmail.com>
2020-12-30 14:33:56 +0530

    Added User profile feed api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,7 @@ router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 #with queryset ypu dont need base_name
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
